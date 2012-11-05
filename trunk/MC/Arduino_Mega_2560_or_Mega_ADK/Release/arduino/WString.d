0a55cc785a4d037a98e8907cdc4ee735ae00ba00
pcaaronf@gmail.com <pcaaronf@gmail.com@400c6a48-2095-d90f-62be-5faaa640f4ee>
2012-11-05 14:22:32 +0000

    MC: added some ignore folders

--- trunk/MC/Arduino_Mega_2560_or_Mega_ADK/Release/arduino/WString.d
+++
@@ -1,5 +0,0 @@
-arduino/WString.d arduino/WString.o:  \
- D:/01_Master_TUM/10_Teambots/arduino-1.0.1-windows/arduino-1.0.1/hardware/arduino/cores/arduino/WString.cpp \
-  D:/01_Master_TUM/10_Teambots/arduino-1.0.1-windows/arduino-1.0.1/hardware/arduino/cores/arduino/WString.h
-
-D:/01_Master_TUM/10_Teambots/arduino-1.0.1-windows/arduino-1.0.1/hardware/arduino/cores/arduino/WString.h:
--- trunk/MC/Arduino_Mega_2560_or_Mega_ADK/Release/arduino/new.d
+++
@@ -1,5 +0,0 @@
-arduino/new.d arduino/new.o:  \
- D:/01_Master_TUM/10_Teambots/arduino-1.0.1-windows/arduino-1.0.1/hardware/arduino/cores/arduino/new.cpp \
-  D:\01_Master_TUM\10_Teambots\arduino-1.0.1-windows\arduino-1.0.1\hardware\arduino\cores\arduino/new.h
-
-D:\01_Master_TUM\10_Teambots\arduino-1.0.1-windows\arduino-1.0.1\hardware\arduino\cores\arduino/new.h:

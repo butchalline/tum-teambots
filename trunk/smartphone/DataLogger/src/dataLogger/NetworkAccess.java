8bc34766b35e2002a84dd4e9e0c166b7d2505671
reimannalex88 <reimannalex88@400c6a48-2095-d90f-62be-5faaa640f4ee>
2012-11-26 15:09:46 +0000

    Smartphone: -Proxyname support added

--- trunk/smartphone/DataLogger/src/dataLogger/NetworkAccess.java
+++ trunk/smartphone/DataLogger/src/dataLogger/NetworkAccess.java
@@ -1,24 +1,33 @@
 package dataLogger;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import teambotData.ByteArrayData;
 import teambotData.FloatArrayData;
 import Communication.*;
 
 public class NetworkAccess extends Ice.Application implements IMemoryAccess {
 	
-	String hostname = "localhost";
-	String port = "10000";
+	public AtomicBoolean running = new AtomicBoolean(false);
+	
+	private String hostname = "localhost";
+	private String port = "10000";
+	private String proxyName = "DataLogger";	
+	private Ice.Communicator communicator;
 	
 	protected DataInterfacePrx networkDataProxy;
 	
-	NetworkAccess()
+	public NetworkAccess()
 	{
+		running = new AtomicBoolean(false);
 	}
 	
-	public NetworkAccess(String hostnameOrIp, String port)
+	public NetworkAccess(String hostnameOrIp, String port, String proxyName)
 	{
+		running = new AtomicBoolean(false);
 		this.hostname = hostnameOrIp;
 		this.port = port;
+		this.proxyName = proxyName;
 	}
 	
 	@Override
@@ -51,6 +60,8 @@ public class NetworkAccess extends Ice.Application implements IMemoryAccess {
 
 	@Override
 	public void run() {        
+		if(running.get())
+			return;
 	    String args[] = { "" };
 	    main("CommunicationServer", args);
 	}
@@ -65,23 +76,33 @@ public class NetworkAccess extends Ice.Application implements IMemoryAccess {
 //			configFile.load(this.getClass().getClassLoader().getResourceAsStream("/IceServer.properties"));
 //			String host = configFile.getProperty("host");
 //			String port = configFile.getProperty("port");
-//			prx = communicator().stringToProxy("DataLogger:tcp -h "+ host +" -p " + port).ice_twoway();
+//			prx = communicator.stringToProxy("DataLogger:tcp -h "+ host +" -p " + port).ice_twoway();
 //			
 //		} catch (IOException e) {
-//			prx = communicator().stringToProxy("DataLogger:tcp -h localhost -p 10000").ice_twoway();
+//			prx = communicator.stringToProxy("DataLogger:tcp -h localhost -p 10000").ice_twoway();
 //			
 //			e.printStackTrace();
 //		}        
 		        
-        Ice.ObjectPrx prx = communicator().stringToProxy("DataLogger:udp -h "+ hostname + " -p "+ port).ice_datagram();
+		Ice.StringSeqHolder argsH = new Ice.StringSeqHolder(args);
+		Ice.Properties properties = Ice.Util.createProperties(argsH);
+//		properties.setProperty("Ice.Warn.Connections", "2");
+//		properties.setProperty("Ice.Trace.Protocol", "2");
+		Ice.InitializationData id = new Ice.InitializationData();
+		id.properties = properties;
+		communicator = Ice.Util.initialize(id);
+		running.set(true);
+		
+        Ice.ObjectPrx prx = communicator.stringToProxy(proxyName +":udp -h "+ hostname + " -p "+ port).ice_datagram();
         networkDataProxy = Communication.DataInterfacePrxHelper.uncheckedCast(prx);
-        communicator().waitForShutdown();
+        communicator.waitForShutdown();
         return 0;
 	}
 
 	@Override
 	public void stop() {
-		communicator().shutdown();	
+		running.set(false);
+		communicator.shutdown();	
 	}
 
 }

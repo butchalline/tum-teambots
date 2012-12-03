949f7783d3d9a3405a64de8eb8b50ba2171a9e26
reimannalex88 <reimannalex88@400c6a48-2095-d90f-62be-5faaa640f4ee>
2012-12-03 17:32:30 +0000

    Smartphone: - Nexus sensor list added - Fizzeled around with Windows Ice log server thingy

--- trunk/Windows/TeambotLogIceServer/TeambotLogIceServer/DataLogger.h
+++ trunk/Windows/TeambotLogIceServer/TeambotLogIceServer/DataLogger.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <vector>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #include "DataInterface.h"
 
 class DataLogger : public Communication::DataInterface
 {
 public:
-	virtual void sendData(const Communication::DataPtr& data, const Ice::Current&);
+	virtual void sendByteData(const Communication::ByteDataPtr& data, const Ice::Current&);
+	virtual void sendFloatData(const Communication::FloatDataPtr& data, const Ice::Current&);
+
+private:
+	cv::Mat image;
 };
--- trunk/Windows/TeambotLogIceServer/TeambotLogIceServer/LogIceServer.cpp
+++ trunk/Windows/TeambotLogIceServer/TeambotLogIceServer/LogIceServer.cpp
@@ -1,6 +1,8 @@
 
 #include <Ice/Ice.h>
+#include "DataInterface.h"
 #include "DataLogger.h"
+#include <vector>
 
 using namespace std;
 
@@ -8,32 +10,53 @@ int main(int argc, char* argv[])
 {
     Ice::CommunicatorPtr communicator;
 
-    try
-    {
-        communicator = Ice::initialize(argc, argv);
-        Ice::ObjectAdapterPtr adapter = communicator->createObjectAdapterWithEndpoints("LogServer", "udp -p 10000");
-        adapter->add(new DataLogger, communicator->stringToIdentity("DataLogger"));
-        adapter->activate();
+    //try
+    //{
+		Ice::PropertiesPtr props = Ice::createProperties(argc, argv);
+		props->setProperty("Ice.Trace.Network", "2");
+		props->setProperty("Ice.Trace.Protocol", "2");
+		Ice::InitializationData id;
+		id.properties = props;
+
+        communicator = Ice::initialize(id);
+
+		//Ice::ObjectAdapterPtr adapter = communicator->createObjectAdapterWithEndpoints("ControlServer", "udp -p 9030");
+		//adapter->add(new DataLogger, communicator->stringToIdentity("Logger"))->ice_datagram();
+		//adapter->activate();
+
+		Communication::DataInterfacePrx botProxy = Communication::DataInterfacePrx::uncheckedCast(
+			communicator->stringToProxy("InterfaceBot1:tcp -h 131.159.195.64 -p 9004"));//->ice_datagram());
 
 		cout << "DataLogger server ready." << endl;
+		vector<byte> byteData;
+		byteData.push_back(1);
+		byteData.push_back(45);
+		byteData.push_back(23);
+		byteData.push_back(25);
+		byteData.push_back(120);
+		byteData.push_back(42);
+
+		Communication::ByteData* testData = new Communication::ByteData(Communication::DataTypeIce::ACCELEROMETER, 0, byteData);
+		botProxy->sendByteData(testData);
+
         communicator->waitForShutdown();
         communicator->destroy();
-    }
-    catch(const Ice::Exception& ex)
-    {
-        cerr << ex << endl;
-        if(communicator)
-        {
-            try
-            {
-                communicator->destroy();
-            }
-            catch(const Ice::Exception& ex)
-            {
-                cerr << ex << endl;
-            }
-        }
-        exit(1);
-    }
+   // }
+    //catch(const Ice::Exception& ex)
+    //{
+    //    cerr << ex << endl;
+    //    if(communicator)
+    //    {
+    //        try
+    //        {
+    //            communicator->destroy();
+    //        }
+    //        catch(const Ice::Exception& ex)
+    //        {
+    //            cerr << ex << endl;
+    //        }
+    //    }
+    //    exit(1);
+    //}
     return 0;
 }

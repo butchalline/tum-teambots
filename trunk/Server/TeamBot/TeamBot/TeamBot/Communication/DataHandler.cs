565e32a5ef16dd84149184426442641914d1a480
pcaaronf@gmail.com <pcaaronf@gmail.com@400c6a48-2095-d90f-62be-5faaa640f4ee>
2013-01-07 10:58:23 +0000

    DataI.ice created

--- trunk/Server/TeamBot/TeamBot/TeamBot/Communication/DataHandler.cs
+++ trunk/Server/TeamBot/TeamBot/TeamBot/Communication/DataHandler.cs
@@ -17,17 +17,48 @@ namespace TeamBot.Communication
     }
 
 
-    class DataHandler : IData
+    class DataHandler : IDataDisp_
     {
         private Robot _Bot;
         private int _Time;
+        private IDataPrx _Proxy;
+        private Ice.Communicator _Communicator;
 
         public DataHandler(Robot robot)
         {
             _Bot = robot;
+            Ice.Properties props = Ice.Util.createProperties();
+            props.setProperty("Ice.Warn.Connections", "2");
+            props.setProperty("Ice.Trace.Protocol", "2");
+            Ice.InitializationData initData = new Ice.InitializationData();
+            initData.properties = props;
+            _Communicator = Ice.Util.initialize(initData);
         }
 
-        public void receive(TBFrame frame)
+        public void sendInfraredFrame(byte left, byte middle, byte right)
+        {
+            TBInfraredData infraredData = new TBInfraredData();
+            infraredData.Id = Constants.TB_DATA_ID;
+            infraredData.SubId = Constants.TB_DATA_INFRARED;
+            infraredData.TimeStamp = 0;
+            infraredData.leftSpeed = left;
+            infraredData.middletSpeed = middle;
+            infraredData.rightSpeed = right;
+            sendData(infraredData);
+        }
+
+        internal void update(GameTime gameTime)
+        {
+            _Time += gameTime.ElapsedGameTime.Milliseconds;
+
+            if (_Time > 100)
+            {
+                _Time = 0;
+                sendInfraredFrame(_Bot.getLeftSensorDistance(), _Bot.getMiddleSensorDistance(), _Bot.getRightSensorDistance());
+            }
+        }
+
+        public override void receive(TBFrame frame, Ice.Current current__)
         {
             switch (frame.Id)
             {
@@ -36,8 +67,8 @@ namespace TeamBot.Communication
                 case Constants.TB_VELOCITY_ID:
                     if (!(frame is TBVelocity))
                         throw new Exception("Frame is not a Velocity Frame");
-                    TBVelocity data = (TBVelocity) frame;
-                    
+                    TBVelocity data = (TBVelocity)frame;
+
                     if (data.SubId == Constants.TB_VELOCITY_FORWARD)
                     {
                         _Bot.setVelocity(data.speedLeft * 4, data.speedRight * 4, WheelDirection.Forwards, WheelDirection.Forwards);
@@ -54,44 +85,37 @@ namespace TeamBot.Communication
                     {
                         _Bot.setVelocity(data.speedLeft * 4, data.speedRight * 4, WheelDirection.Forwards, WheelDirection.Backwards);
                     }
-                    
                     break;
                 case Constants.TB_POSITION_ID:
                     break;
                 case Constants.TB_ERROR_ID:
-                    break;                
+                    break;
             }
-        }
-
-
-        
-
-        public void sendInfraredFrame(byte left, byte middle, byte right)
-        {
-            TBInfraredData infraredData = new TBInfraredData();
-            infraredData.Id = Constants.TB_DATA_ID;
-            infraredData.SubId = Constants.TB_DATA_INFRARED;
-            infraredData.TimeStamp = 0;
-            infraredData.leftSpeed = left;
-            infraredData.middletSpeed = middle;
-            infraredData.rightSpeed = right;
-            Console.WriteLine("InfraredData : " + infraredData.leftSpeed.ToString() + " | " + infraredData.middletSpeed + " | " + infraredData.rightSpeed.ToString());
-            sendData(infraredData);
         }
 
         public void sendData(TBFrame data)
         {
-            //TODO Implement send data via ice
-        }
-
-        internal void update(GameTime gameTime)
-        {
-            _Time += gameTime.ElapsedGameTime.Milliseconds;
-
-            if (_Time > 100)
+            if (_Communicator.isShutdown())
             {
-                _Time = 0;
-                sendInfraredFrame(_Bot.getLeftSensorDistance(), _Bot.getMiddleSensorDistance(), _Bot.getRightSensorDistance());
+                Ice.Properties props = Ice.Util.createProperties();
+                props.setProperty("Ice.Warn.Connections", "2");
+                props.setProperty("Ice.Trace.Protocol", "2");
+                Ice.InitializationData initData = new Ice.InitializationData();
+                initData.properties = props;
+                _Communicator = Ice.Util.initialize(initData);
+                _Proxy = null;
+            }
+            if (_Proxy == null)
+            {
+                _Proxy = IDataPrxHelper.uncheckedCast(_Communicator.stringToProxy("hello:tcp -p 10000").ice_oneway());
+            }
+            try
+            {
+                _Proxy.receive(data);
+            }
+            catch(Exception e)
+            {
+            //   Console.WriteLine("Error DataHandler: " + e.Message);
             }
         }
     }
---
+++ trunk/Server/TeamBot/TeamBot/TeamBot/Communication/IData.ice
@@ -0,0 +1,31 @@
+﻿module TeamBot
+{
+	module Communication
+	{
+
+		class TBFrame
+		{
+			byte Id; //defined above
+			byte SubId;
+			short TimeStamp; //in 10ms
+		};
+
+		class TBVelocity extends TBFrame
+		{
+			byte speedLeft;
+			byte speedRight;
+		};
+
+		class TBInfraredData extends TBFrame
+		{
+			byte leftSpeed;
+			byte middletSpeed;
+			byte rightSpeed;
+		};
+
+		interface IData
+		{
+			void receive(TBFrame vel);
+		};
+	};
+ };

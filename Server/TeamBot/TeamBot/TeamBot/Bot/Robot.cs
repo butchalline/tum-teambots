15970e425a26bfe5a1ea8fb4a6ce9f05de9f0745
ReimannAlex88@gmail.com <ReimannAlex88@gmail.com@400c6a48-2095-d90f-62be-5faaa640f4ee>
2013-03-14 17:39:11 +0000

    Server: -Changed base velocity on position mode from 1000 to 400 -Fixed a bug in target position calculation (in setPosition()) -Added Locks to make the whole thing thread safe (needed because of ICE)

--- Server/TeamBot/TeamBot/TeamBot/Bot/Robot.cs
+++ Server/TeamBot/TeamBot/TeamBot/Bot/Robot.cs
@@ -32,7 +32,8 @@ namespace teambot.Bot
         public const int hexRPM = 1023;
         public const float scaleFactor = .23f;
         public const float SensorToHalfSizeRelation = 0.7f; //(Texture.Size / 2) * Position relative to middle...
-        private const float _Threshold = 0.1f;
+        private const float _Threshold = 0.01f;
+        public readonly object _lockObject = new object();
 
         private RobotStates _RobotState = RobotStates.Idle;
 
@@ -122,97 +123,100 @@ namespace teambot.Bot
         }
         internal void update(GameTime gameTime)
         {
-            if (_RobotState == RobotStates.PositionMode)
+            lock (_lockObject)
             {
-                Vector2 t;
-                float velocity = 400;
-                if (!targetPositionReached(out t))
+                if (_RobotState == RobotStates.PositionMode)
                 {
-                    PositionReached = false;
-                    if (t.Length() <= 10)
-                        velocity = 400 - ((4 - t.Length()) * 100);
-                    if (velocity < 1)
-                        velocity = 1;
-                    if (_PositionForward)
+                    Vector2 t;
+                    float velocity = 400;
+                    if (!targetPositionReached(out t))
                     {
-                        _vLeft = (int)velocity;
-                        _vRight = (int)velocity;
+                        PositionReached = false;
+                        if (t.Length() <= 4)
+                            velocity = 400 - ((4 - t.Length()) * 100);
+                        if (velocity < 1)
+                            velocity = 1;
+                        if (_PositionForward)
+                        {
+                            _vLeft = (int)velocity;
+                            _vRight = (int)velocity;
+                        }
+                        else
+                        {
+                            _vLeft = (int)-velocity;
+                            _vRight = (int)-velocity;
+                        }
+                    }
+                    else if (!targetAngleReached())
+                    {
+                        PositionReached = false;
+                        float a = _TargetAngle - Angle;
+
+                        a = a > (float)Math.PI ? a - 2f * (float)Math.PI : a;
+                        a = a < -(float)Math.PI ? a + 2f * (float)Math.PI : a;
+
+                        if (Math.Abs(a) <= MathHelper.ToRadians(4))
+                            velocity = 400 - ((4 - Math.Abs(MathHelper.ToDegrees(a))) * 100);
+                        if (velocity < 1)
+                            velocity = 1;
+                        if (_AngleClockwise)
+                        {
+                            _vLeft = (int)velocity;
+                            _vRight = (int)-velocity;
+                        }
+                        else
+                        {
+                            _vLeft = (int)-velocity;
+                            _vRight = (int)velocity;
+                        }
                     }
                     else
                     {
-                        _vLeft = (int)-velocity;
-                        _vRight = (int)-velocity;
+                        _vLeft = 0;
+                        _vRight = 0;
+                        if (PositionRequested == true)
+                        {
+                            PositionRequested = false;
+                            PositionReached = true;
+                        }
                     }
                 }
-                else if (!targetAngleReached())
-                {
-                    PositionReached = false;
-                    float a = _TargetAngle - Angle;
 
-                    a = a > (float)Math.PI ? a - 2f * (float)Math.PI : a;
-                    a = a < -(float)Math.PI ? a + 2f * (float)Math.PI : a;
 
-                    if (Math.Abs(a) <= MathHelper.ToRadians(10))
-                        velocity = 400 - ((4 - Math.Abs(MathHelper.ToDegrees(a))) * 100);
-                    if (velocity < 1)
-                        velocity = 1;
-                    if (_AngleClockwise)
-                    {
-                        _vLeft = (int)velocity;
-                        _vRight = (int)-velocity;
-                    }
-                    else
-                    {
-                        _vLeft = (int)-velocity;
-                        _vRight = (int)velocity;
-                    }
-                }
-                else
-                {
-                    _vLeft = 0;
-                    _vRight = 0;
-                    if (PositionRequested == true)
-                    {
-                        PositionRequested = false;
-                        PositionReached = true;
-                    }
-                }
+                /**
+                 * Strecke
+                 * (dR + dL) / 2
+                 * Winkel
+                 * (dR - dL) / Achsabstand
+                 * 
+                 * x = x + strecke * cos(winkel)
+                 * y = y + strecke * sin(winkel)
+                 * winkel = winkel + winkel
+                 */
+
+                //Page 18 dynamixel documentation... 0x3ff speed = 114 RPM ( devided by 60 = rounds per second)
+                double dR = 0.5f * gameTime.ElapsedGameTime.TotalSeconds * WheelPerimeter * ((_vRight * (114f / 1023f)) / 60f);
+                double dL = 0.5f * gameTime.ElapsedGameTime.TotalSeconds * WheelPerimeter * ((_vLeft * (114f / 1023f)) / 60f);
+                double s = (dR + dL) / 2;
+                double w = (dR - dL) / AxialDistance;
+
+                Angle -= (float)w;
+                if (Angle < -Math.PI)
+                    Angle += (float)(2 * Math.PI);
+                else if (Angle > Math.PI)
+                    Angle -= (float)(2 * Math.PI);
+
+                _Position.X += (float)(s * Math.Sin(Angle));
+                _Position.Y -= (float)(s * Math.Cos(Angle));
+
+
+                _CurrentLeftSensorDistance = _InfraSensor.checkLeftSensor(Position, (float)Angle);
+                _CurrentMiddleSensorDistance = _InfraSensor.checkMiddleSensor(Position, (float)Angle);
+                _CurrentRightSensorDistance = _InfraSensor.checkRightSensor(Position, (float)Angle);
+
+                _DebugIndex = DebugLayer.addString("VLeft: " + this._vLeft.ToString() + "\nVRight: " + this._vRight.ToString() + "\nAngle: " + Math.Round(MathHelper.ToDegrees((float)(-this.Angle + Math.PI * 0.5)), 2).ToString() + "\nRobotPos: \nX: " + Math.Round(this.Position.X * Map.PixelToCm, 2).ToString() + " Y: " + Math.Round(this.Position.Y * Map.PixelToCm, 2).ToString()
+                    + "\nSensoren L|M|R:\n| " + _CurrentLeftSensorDistance.ToString() + " | " + _CurrentMiddleSensorDistance + " | " + _CurrentRightSensorDistance.ToString() + " | ", _DebugIndex);
             }
-
-
-            /**
-             * Strecke
-             * (dR + dL) / 2
-             * Winkel
-             * (dR - dL) / Achsabstand
-             * 
-             * x = x + strecke * cos(winkel)
-             * y = y + strecke * sin(winkel)
-             * winkel = winkel + winkel
-             */
-
-            //Page 18 dynamixel documentation... 0x3ff speed = 114 RPM ( devided by 60 = rounds per second)
-            double dR = 0.5f * gameTime.ElapsedGameTime.TotalSeconds * WheelPerimeter * ((_vRight * (114f / 1023f)) / 60f);
-            double dL = 0.5f * gameTime.ElapsedGameTime.TotalSeconds * WheelPerimeter * ((_vLeft * (114f / 1023f)) / 60f);
-            double s = (dR + dL) / 2;
-            double w = (dR - dL) / AxialDistance;
-
-            Angle -= (float)w;
-            if (Angle < -Math.PI)
-                Angle += (float) (2 * Math.PI);
-            else if (Angle > Math.PI)
-                Angle -= (float) (2 * Math.PI);
-
-            _Position.X += (float)(s * Math.Sin(Angle));
-            _Position.Y -= (float)(s * Math.Cos(Angle));
-
-
-            _CurrentLeftSensorDistance = _InfraSensor.checkLeftSensor(Position, (float)Angle);
-            _CurrentMiddleSensorDistance = _InfraSensor.checkMiddleSensor(Position, (float)Angle);
-            _CurrentRightSensorDistance = _InfraSensor.checkRightSensor(Position, (float)Angle);
-
-            _DebugIndex = DebugLayer.addString("VLeft: " + this._vLeft.ToString() + "\nVRight: " + this._vRight.ToString() + "\nAngle: " + Math.Round(MathHelper.ToDegrees((float)(-this.Angle + Math.PI * 0.5)), 2).ToString() + "\nRobotPos: \nX: " + Math.Round(this.Position.X * Map.PixelToCm, 2).ToString() + " Y: " + Math.Round(this.Position.Y * Map.PixelToCm, 2).ToString()
-                + "\nSensoren L|M|R:\n| " + _CurrentLeftSensorDistance.ToString() + " | " + _CurrentMiddleSensorDistance + " | " + _CurrentRightSensorDistance.ToString() + " | ", _DebugIndex);
         }
 
         internal void setVelocity(int velocityLeft, int veloctiyRight, WheelDirection wheelDirectionLeft, WheelDirection wheelDirectionRight)
@@ -273,17 +277,16 @@ namespace teambot.Bot
 
         internal void setPosition(int p)
         {
-            if (this._RobotState != RobotStates.PositionMode)
-                return;
-            _PositionForward = true;
-            if (p < 0)
-                _PositionForward = false;
+                if (this._RobotState != RobotStates.PositionMode)
+                    return;
+                _PositionForward = true;
+                if (p < 0)
+                    _PositionForward = false;
 
-            double cAngle = Angle + Math.PI;
-             float p2 = p * Map.PixelToCm * 0.1f;
-             _TargetPosition.X = (float)(_Position.X - p2 * Math.Sin(cAngle));
-             _TargetPosition.Y = (float)(_Position.Y + p2 * Math.Cos(cAngle));
-
+                double cAngle = Angle + Math.PI;
+                float p2 = (p * 0.1f) / Map.PixelToCm;
+                _TargetPosition.X = (float)(_Position.X - p2 * Math.Sin(cAngle));
+                _TargetPosition.Y = (float)(_Position.Y + p2 * Math.Cos(cAngle));
         }
 
         internal void setAngle(int p)

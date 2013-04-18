4e4961de22261e0b32337e288bd2ca35973d95dc
reimannalex88 <reimannalex88@400c6a48-2095-d90f-62be-5faaa640f4ee>
2013-04-18 03:38:44 +0000

    Android: -Made simulator interface use asynchronous calls -Changed the ice of the simulator interface to the recent version

---
+++ trunk/smartphone/ISimulator/src/teambot/simulator/SimulatorCommunicator.java
@@ -0,0 +1,116 @@
+package teambot.simulator;
+
+import teambot.common.Bot;
+import teambot.communication.IDataServerPrx;
+import teambot.communication.IDataServerPrxHelper;
+import teambot.communication.TBFrame;
+import teambot.communication._IDataClientDisp;
+import teambot.communication._IDataServerDisp;
+import Ice.Application;
+
+public class SimulatorCommunicator extends Application implements Runnable {
+
+	protected boolean verbose = false;
+	protected Ice.Communicator communicator = null;
+	protected Ice.ObjectAdapter objectAdapter;
+	protected String localPort = "91000";
+	protected String remotePort = "91000";
+	protected String simulatorIp = "";
+	protected boolean useTcp = true;
+	protected IDataServerPrx serverInterface;
+	protected Ice.Identity ident;
+
+	public SimulatorCommunicator(String localPort, String remotePort) {
+		initialize(localPort, remotePort, true, true);
+	}
+
+	public SimulatorCommunicator(String localPort, String remotePort, boolean useTcp, boolean verbose) {
+		initialize(localPort, remotePort, useTcp, verbose);
+	}
+
+	private void initialize(String localPort, String remotePort, boolean useTcp, boolean verbose) {
+		this.localPort = localPort;
+		this.remotePort = remotePort;
+		this.useTcp = useTcp;
+		this.verbose = verbose;
+        ident = new Ice.Identity();
+        ident.name = java.util.UUID.randomUUID().toString();
+        ident.category = "";
+	}
+
+	public void start(_IDataClientDisp localDataInterface, String simulatorIp) {
+
+		new Thread(this).start();
+
+		while (communicator == null) {
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (useTcp) {
+			objectAdapter = communicator.createObjectAdapterWithEndpoints(Bot.getId(), "tcp -h localhost -p " + localPort);
+			objectAdapter.add(localDataInterface, ident);
+			
+		} else {
+			objectAdapter = communicator.createObjectAdapterWithEndpoints(Bot.getId(), "udp -h localhost -p " + localPort);
+			objectAdapter.add(localDataInterface, ident);
+		}
+
+		objectAdapter.activate();
+		this.simulatorIp = simulatorIp;
+	}
+
+	@Override
+	public void run() {
+		String args[] = { "" };
+		main("", args);
+	}
+
+	@Override
+	public int run(String[] args) {
+
+		Ice.StringSeqHolder argsH = new Ice.StringSeqHolder(args);
+		Ice.Properties properties = Ice.Util.createProperties(argsH);
+		if (this.verbose) {
+			properties.setProperty("Ice.Warn.Connections", "2");
+			properties.setProperty("Ice.Trace.Protocol", "2");
+		}
+		Ice.InitializationData initializationData = new Ice.InitializationData();
+		initializationData.properties = properties;
+		communicator = Ice.Util.initialize(initializationData);
+
+		communicator.waitForShutdown();
+		return 0;
+	}
+
+	public synchronized void sendFrame(TBFrame frame) {
+		
+		if (serverInterface == null) {
+			Ice.ObjectPrx prx;
+
+			if (useTcp) {
+				prx = communicator.stringToProxy("Simulator:tcp -h " + simulatorIp + " -p " + remotePort);
+			} else {
+				prx = communicator.stringToProxy("Simulator:udp -h " + simulatorIp + " -p " + remotePort);
+			}
+
+			serverInterface = IDataServerPrxHelper.uncheckedCast(prx);
+			serverInterface.ice_getConnection().setAdapter(objectAdapter);
+			serverInterface.addClient(ident);
+		}
+		
+		while(serverInterface == null) 
+		{//wait for remoteInterface initialization
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	
+		serverInterface.update(frame);
+	}
+}
--- trunk/smartphone/ISimulator/src/teambot/simulator/SimulatorProxy.java
+++ trunk/smartphone/ISimulator/src/teambot/simulator/SimulatorProxy.java
@@ -1,53 +1,109 @@
 package teambot.simulator;
 
-import java.io.IOException;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import teambot.common.NetworkConnection;
-import teambot.common.data.ByteArrayData;
-import teambot.common.data.DataType;
-import teambot.common.data.IceDataMapper;
-import teambot.common.interfaces.IUsbIO;
-import Communication.ByteData;
-import Communication.DataInterfacePrx;
-import Communication.FloatData;
-import Communication._DataInterfaceDisp;
+import teambot.communication.AMD_IDataClient_update;
+import teambot.communication.TBFrame;
+import teambot.communication.TBInfraredData;
+import teambot.communication.TBPositionData;
+import teambot.communication.TBPositionReached;
+import teambot.communication._IDataClientDisp;
 import Ice.Current;
 
-public class SimulatorProxy extends _DataInterfaceDisp implements IUsbIO {
+public class SimulatorProxy extends _IDataClientDisp {
 
-	NetworkConnection networkConnection = new NetworkConnection();
-	DataInterfacePrx simulator;
-	ConcurrentLinkedQueue<byte[]> bufferInput;
-	
-	public SimulatorProxy(String ip, String port) {
-		networkConnection.addLocalUdpProxy(this, "USB", "7000");
-		simulator = networkConnection.connectToRemoteTcpProxy("SimulatorUSB", ip, port);
+	private static final long serialVersionUID = -1726784146178972520L;
+
+	protected TBInfraredData infraredData;
+	protected TBPositionData positionData;
+	protected TBInfraredData lastInfraredData;
+	protected TBPositionData lastPositionData;
+	protected SimulatorCommunicator simulatorInterface;
+	protected boolean isPositionReached = false;
+	protected short lastTimeStamp = 0;
+	protected boolean lastPackageWasPosition = false;
+
+	public class PositionInfraredData {
+		public TBPositionData positionData;
+		public TBInfraredData infraredData;
+	}
+
+	public void start(String ip, String localPort, String remotePort) {
+		simulatorInterface = new SimulatorCommunicator(localPort, remotePort, true, false);
+		simulatorInterface.start(this, ip);
 	}
 	
 	@Override
-	public int read(byte[] b) throws IOException {
-		b = bufferInput.poll();
-		
-		if(b == null)
-			return 0;
-		
-		return b.length;
+	public void update_async(AMD_IDataClient_update __cb, TBFrame data, Current __current) {
+
+		if (data instanceof TBPositionData) {
+			lastPositionData = (TBPositionData) data;
+		} else if (data instanceof TBPositionReached) {
+			TBPositionReached reachedFrame = (TBPositionReached) data;
+			setPositionReached();
+			lastPositionData = (new TBPositionData(reachedFrame.Id, reachedFrame.SubId, reachedFrame.TimeStamp, reachedFrame.x,
+					reachedFrame.y, reachedFrame.angle));
+		} else if (data instanceof TBInfraredData) {
+			lastInfraredData = (TBInfraredData) data;
+		}
+
+		if ((data instanceof TBPositionData || data instanceof TBPositionReached) && data.TimeStamp == lastTimeStamp
+				&& !lastPackageWasPosition) {
+			updateValues();
+		} else if (data instanceof TBInfraredData && data.TimeStamp == lastTimeStamp && lastPackageWasPosition)
+			updateValues();
+
+		if (data instanceof TBInfraredData)
+			lastPackageWasPosition = false;
+		else if (data instanceof TBPositionData || data instanceof TBPositionReached)
+			lastPackageWasPosition = true;
+
+		lastTimeStamp = data.TimeStamp;
+		__cb.ice_response();
 	}
 
-	@Override
-	public void write(byte[] b) throws IOException {
-		simulator.sendByteData(IceDataMapper.map(new ByteArrayData(b, DataType.SIMULATOR)));
+	private synchronized void updateValues() {
+		infraredData = lastInfraredData;
+		positionData = lastPositionData;
 	}
 
-	@Override
-	public void sendByteData(ByteData data, Current __current) {
-		bufferInput.add(data.byteArrayData);
+	public synchronized PositionInfraredData getLastMeasurement() {
+		PositionInfraredData data = new PositionInfraredData();
+		data.infraredData = infraredData;
+		data.positionData = positionData;
+		return data;
 	}
 
-	@Override
-	public void sendFloatData(FloatData data, Current __current) {
-
+	public synchronized void setPositionReached() {
+		isPositionReached = true;
 	}
 
+	public synchronized boolean positionIsReached() {
+		if (!isPositionReached)
+			return false;
+
+		isPositionReached = false;
+		return true;
+	}
+
+	public void send(TBFrame frame) {
+		try {
+			simulatorInterface.sendFrame(frame);
+		} catch (Exception ex) {
+
+		}
+	}
+
+	public void sendBlocking(TBFrame frame) {
+		boolean sentSuccessfull = false;
+
+		while (!sentSuccessfull) {
+			try {
+				simulatorInterface.sendFrame(frame);
+				sentSuccessfull = true;
+			} catch (Ice.ConnectionRefusedException ex) {
+
+			} catch (Ice.ConnectFailedException ex) {
+
+			}
+		}
+	}
 }

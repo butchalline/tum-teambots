dc9dbab930ccd212550bbe27f000ad6ed2f69010
reimannalex88 <reimannalex88@400c6a48-2095-d90f-62be-5faaa640f4ee>
2013-08-04 10:57:28 +0000

    Java: -slam works

--- trunk/smartphone/Slam/src/teambot/slam/BeamModel.java
+++ trunk/smartphone/Slam/src/teambot/slam/BeamModel.java
@@ -4,6 +4,8 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.LinkedList;
 import java.util.Vector;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import teambot.common.data.Position;
 import teambot.pathplanning.Occupation;
 import android.graphics.Point;
@@ -34,22 +36,38 @@ public class BeamModel
 
 		rayEnd.x = (float) (position.getX() + Math.cos(position.getAngleInRadian()) * distance_mm);
 		rayEnd.y = (float) (position.getY() + Math.sin(position.getAngleInRadian()) * distance_mm);
-		
+
 		rayEnd = realToGridCoordinates(rayEnd);
-		PointF	rayStart = realToGridCoordinates(position.getPosition());
+		PointF rayStart = realToGridCoordinates(position.getPosition());
 
 		LinkedList<SimpleEntry<Point, Occupation>> pointsOnBeam = new LinkedList<SimpleEntry<Point, Occupation>>();
 
-		if (distance_mm < _maxRange_mm)
+		// if (distance_mm < _maxRange_mm)
+		// {
+		// Point rayEndDiscretized = new Point();
+		//
+		// if(position.getAngleInDegree() > 0)
+		// rayEndDiscretized.y = (int)rayEnd.y;
+		// else
+		// rayEndDiscretized.y = (int)Math.ceil(rayEnd.y);
+		//
+		// if(position.getAngleInDegree() < 90 && position.getAngleInDegree() >
+		// -90)
+		// rayEndDiscretized.x = (int)rayEnd.x;
+		// else
+		// rayEndDiscretized.x = (int)Math.ceil(rayEnd.x);
+		//
+		// pointsOnBeam.add(new SimpleEntry<Point,
+		// Occupation>(rayEndDiscretized, Occupation.occupied));
+		// }
+
+		for (Object freePoint : calculateFreePoints(distance_mm, rayStart, rayEnd))
 		{
-			Point rayEndDiscretized = new Point((int) (rayEnd.x), (int) (rayEnd.y));
-			pointsOnBeam.add(new SimpleEntry<Point, Occupation>(rayEndDiscretized, Occupation.occupied));
+			pointsOnBeam.add(new SimpleEntry<Point, Occupation>((Point)freePoint, Occupation.free));
 		}
 
-		for (Point freePoint : calculateFreePoints(distance_mm, rayStart, rayEnd))
-		{
-			pointsOnBeam.add(new SimpleEntry<Point, Occupation>(freePoint, Occupation.free));
-		}
+		if(distance_mm < _maxRange_mm)
+			pointsOnBeam.getLast().setValue(Occupation.occupied);
 
 		return pointsOnBeam;
 	}
@@ -63,41 +81,47 @@ public class BeamModel
 	{
 		return new PointF(point.x / _cellSize_mm, point.y / _cellSize_mm);
 	}
-	
-	public LinkedList<SimpleEntry<Point, Occupation>> realToGridCoordinates(LinkedList<SimpleEntry<Point, Occupation>> points)
+
+	public LinkedList<SimpleEntry<Point, Occupation>> realToGridCoordinates(
+			LinkedList<SimpleEntry<Point, Occupation>> points)
 	{
 		LinkedList<SimpleEntry<Point, Occupation>> gridPoints = new LinkedList<SimpleEntry<Point, Occupation>>();
-		
+
 		Point point;
-		
+
 		for (SimpleEntry<Point, Occupation> pointSet : points)
 		{
 			point = pointSet.getKey();
-			gridPoints.add(new SimpleEntry<Point, Occupation>(new Point(point.x / (int)_cellSize_mm, point.y / (int)_cellSize_mm), pointSet.getValue()));
+			gridPoints.add(new SimpleEntry<Point, Occupation>(new Point(point.x / (int) _cellSize_mm, point.y
+					/ (int) _cellSize_mm), pointSet.getValue()));
 		}
-		
+
 		return gridPoints;
 	}
 
-	private Vector<Point> calculateFreePoints(float distance, PointF rayStart, PointF rayEnd)
+	private Object[] calculateFreePoints(float distance, PointF rayStart, PointF rayEnd)
 	{
 		float deltaX = rayEnd.x - rayStart.x;
 		float deltaY = rayEnd.y - rayStart.y;
+		boolean reverseArray = false;
 
 		Vector<Point> freePoints = new Vector<Point>();
 		if (Math.abs(deltaX) > Math.abs(deltaY))
 		{
 			float a = deltaY / deltaX;
 			float b = rayStart.y - a * rayStart.x;
-			int min_x = (int) Math.min(rayStart.x, rayEnd.x) + 1;
-			int max_x = (int) Math.max(rayStart.x, rayEnd.x) - 1;
+			int min_x = (int) Math.floor(Math.min(rayStart.x, rayEnd.x));
+			int max_x = (int) Math.floor(Math.max(rayStart.x, rayEnd.x));
+
+			if (rayStart.x > rayEnd.x)
+				reverseArray = true;
 
 			for (int x = min_x; x <= max_x; x++)
 			{
-				int y = (int) (a * x + b);
+				int y = (int) Math.floor(a * x + b);
 
 				Point newLeftField = new Point(x - 1, y);
-				if (freePoints.isEmpty() || freePoints.lastElement().y != newLeftField.y)
+				if (!freePoints.isEmpty() && freePoints.lastElement().y != newLeftField.y)
 				{
 					freePoints.add(newLeftField);
 				}
@@ -110,15 +134,18 @@ public class BeamModel
 		{
 			float a = deltaX / deltaY;
 			float b = rayStart.x - a * rayStart.y;
-			int min_y = (int) Math.min(rayStart.y, rayEnd.y) + 1;
-			int max_y = (int) Math.max(rayStart.y, rayEnd.y) - 1;
+			int min_y = (int) Math.floor(Math.min(rayStart.y, rayEnd.y));
+			int max_y = (int) Math.floor(Math.max(rayStart.y, rayEnd.y));
+
+			if (rayStart.y > rayEnd.y)
+				reverseArray = true;
 
 			for (int y = min_y; y <= max_y; y++)
 			{
-				int x = (int) (a * y + b);
+				int x = (int) Math.floor(a * y + b);
 
 				Point newBottomField = new Point(x, y - 1);
-				if (freePoints.isEmpty() || freePoints.lastElement().x != newBottomField.x)
+				if (!freePoints.isEmpty() && freePoints.lastElement().x != newBottomField.x)
 				{
 					freePoints.add(newBottomField);
 				}
@@ -127,7 +154,12 @@ public class BeamModel
 			}
 		}
 
-		return freePoints;
+		Object[] freePointsArray = freePoints.toArray();
+
+		if(reverseArray)
+			ArrayUtils.reverse(freePointsArray);
+
+		return freePointsArray;
 	}
 
 	public float getCellSize()

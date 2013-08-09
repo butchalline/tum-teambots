18cbccbb66fb4cc0090dd0144b60e3c282c7e08d
reimannalex88 <reimannalex88@400c6a48-2095-d90f-62be-5faaa640f4ee>
2013-08-09 23:27:51 +0000

    Java: -SimpleThread renamed into SimpleEndlessThread to make clear that it runs a loop till the stop method is called -Changed accessory_filter.xml so it matches the things in the mc files.. -Remade the UsbConnection and added it to Teambot project. It doesn't use the intent (and with that the accessory file) but just checks regularly if there is any (and not just the one specified in that xml) accessory connected. Has the advantage that the accessory doesn't need to be connected while the application is running (it can be before) -Added a SettingsAndroid class

--- trunk/smartphone/Teambot/src/teambot/teambot/TeambotActivity.java
+++ trunk/smartphone/Teambot/src/teambot/teambot/TeambotActivity.java
@@ -1,5 +1,8 @@
 package teambot.teambot;
 
+import java.util.concurrent.ExecutionException;
+
+import teambot.usb.UsbConnectionManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +10,7 @@ import android.view.Menu;
 public class TeambotActivity extends Activity
 {
 	private TeambotAsyncTask _teambotTask;
+	private UsbConnectionManager _usbConnectionManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -14,16 +18,32 @@ public class TeambotActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_teambot);
 
-		_teambotTask = new TeambotAsyncTask();
+		_usbConnectionManager = new UsbConnectionManager(this);
+		
+		_usbConnectionManager.start();
+		
+		_teambotTask = new TeambotAsyncTask(_usbConnectionManager);
 		String ip = IpHelper.getIPAddress();
 
 		if (ip == "")
 		{
 			System.out.println("Seems like the network interface has no ip -> turn WLAN on");
+			return;
 		}
-		
-		_teambotTask.execute(ip);
 
+		_teambotTask.execute(ip);
+		
+		try
+		{
+			_teambotTask.get();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		} catch (ExecutionException e)
+		{
+			e.printStackTrace();
+		}
+		_usbConnectionManager.stop();
 	}
 
 	@Override
---
+++ trunk/smartphone/Teambot/src/teambot/usb/IUsbCommunication.java
@@ -0,0 +1,7 @@
+package teambot.usb;
+
+public interface IUsbCommunication
+{
+	public void setupCommunication();
+	public void closeCommunication();
+}
